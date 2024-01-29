0d6c8ee6c18c2c71942fbf90e4f5738d593b9ef1
nvannt <ngayaythatdep2512@gmail.com>
2024-01-29 20:41:45 +0700

    Slide40.C2

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Slide40 {
+    public static void main (String args [] ) {
+        Scanner sc =newScanner(System.in);
+        int ngay =sc.nextInt();
+        switch(ngay) {
+            Case 0 :System.out.println("Chu nhat");
+            break;
+            Case 1 :System.out.println("Thu hai");
+            break;
+            Case 2 :System.out.println("Thu ba");
+            break;
+            Case 3 :System.out.println("Thu tu");
+            break;
+            Case 4 :System.out.println("Thu nam");
+            break;
+            Case 5 :System.out.println("Thu sau");
+            break;
+            Case 6 :System.out.println("Thu bay");
+            break;
+            default:System.out.println("So ngay trong tuan sai");
+            }
+    }
+}
